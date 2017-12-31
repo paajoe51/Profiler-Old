2a4ef1ebe303b2b6100c9b5e934ef0efd4a6bfe3
Michael Abordzi <paajoe51@gmail.com>
2017-12-31 17:34:53 +0000

    fixed the issues withh change of page

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,12 +4,11 @@ import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
 
 import { HomePage } from '../pages/home/home';
-import { IntroPage } from '../pages/intro/intro';
 @Component({
   templateUrl: 'app.html'
 })
 export class MyApp {
-  rootPage:any = IntroPage;
+  rootPage:any = "IntroPage";
 
   constructor(platform: Platform, statusBar: StatusBar, splashScreen: SplashScreen) {
     platform.ready().then(() => {
